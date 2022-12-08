bb349ba0b5748e5ab686a3bf9b9b34f6bec578e6
Antonio Alvarado <antonioalvarado@Antonios-MacBook-Pro.local>
2022-12-08 12:41:10 +0100

    pathing

--- smarthealing_app/website/app.py
+++ smarthealing_app/website/app.py
@@ -90,7 +90,7 @@ if tabs =='Dashboard':
 
     fig = px.bar(
         data_frame=more_common, 
-        x=page.index, 
+        x='delete', 
         y=page, 
         custom_data=['count', 'icd9','edad_mean', 'edad_median', 'duration_mean', 'duration_median', 'description']
     )
